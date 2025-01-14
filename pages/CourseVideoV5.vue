ea36c29a8d6de95deae606ef2643267d0d3b3db8
connor moore <konalmg1@gmail.com>
2025-01-14 00:18:08 +0000

    course view almost there

--- pages/CourseVideoV5.vue
+++ pages/CourseVideoV5.vue
@@ -18,18 +18,25 @@
                 </div>
               </TransitionChild>
 
-              <!-- Sidebar component, swap this element with another sidebar if you like -->
-              <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+              <!-- Mobile Sidebar -->
+              <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-0 pb-0">
                 <div class="flex h-16 shrink-0 items-center">
-                  <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=white" alt="Your Company" />
+                  <h1 class="text-white text-1xl p-6">JavaScript Fundamentals</h1>
                 </div>
+
                 <nav class="flex flex-1 flex-col">
                   <ul role="list" class="flex flex-1 flex-col gap-y-7">
-                    <li>
-                      <ul role="list" class="-mx-2 space-y-1">
+                    <!-- Chapter-->
+                    <li v-for="chapter in chapters" :key="chapter.name">
+                      <p class="bg-gray-700 text-white group flex gap-x-3 p-3 text-sm/6 font-semibold px-6 py-3">
+                        {{  chapter.name }} ({{ chapter.duration }})
+                      </p>
+
+                      <!-- Lessons -->
+                      <ul role="list" class="-mx-2 divide-y divide-gray-800">
                         <li v-for="item in navigation" :key="item.name">
-                          <a :href="item.href" :class="[item.current ? 'bg-gray-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold']">
-                            <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'size-6 shrink-0']" aria-hidden="true" />
+                          <a :href="item.href" :class="[item.current ? 'bg-black text-white' : 'text-gray-400 hover:bg-black hover:text-white', 'group flex gap-x-3 py-3 px-6 text-sm/6 font-semibold']">
+                            <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-gray-200 group-hover:text-white', 'size-6 shrink-0']" aria-hidden="true" />
                             {{ item.name }}
                           </a>
                         </li>
@@ -44,19 +51,26 @@
       </Dialog>
     </TransitionRoot>
 
-    <!-- Static sidebar for desktop -->
+    <!-- Desktop Sidebar -->
     <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
+      <div class="flex grow flex-col gap-y-0 overflow-y-auto bg-gray-900 px-0 pb-4">
         <div class="flex h-16 shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=white" alt="Your Company" />
+          <h1 class="text-white text-1xl p-6">JavaScript Fundamentals</h1>
         </div>
+
         <nav class="flex flex-1 flex-col">
-          <ul role="list" class="flex flex-1 flex-col gap-y-7">
-            <li>
-              <ul role="list" class="-mx-2 space-y-1">
+          <ul role="list" class="flex flex-1 flex-col gap-y-3">
+            <!-- Chapter-->
+            <li v-for="chapter in chapters" :key="chapter.name">
+              <p class="bg-gray-700 text-white group flex gap-x-3 p-3 text-sm/6 font-semibold px-6 py-3">
+                {{  chapter.name }} ({{ chapter.duration }})
+              </p>
+
+              <!-- Lessons -->
+              <ul role="list" class="-mx-2  divide-y divide-gray-800">
+              <!-- <ul role="list" class="space-y-1"> -->
                 <li v-for="item in navigation" :key="item.name">
-                  <a :href="item.href" :class="[item.current ? 'bg-black text-white' : 'text-gray-400 hover:bg-black hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold']">
+                  <a :href="item.href" :class="[item.current ? 'bg-black text-white' : 'text-gray-400 hover:bg-black hover:text-white', 'group flex gap-x-3 py-3 px-6 text-sm/6 font-semibold']">
                     <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-gray-200 group-hover:text-white', 'size-6 shrink-0']" aria-hidden="true" />
                     {{ item.name }}
                   </a>
@@ -69,47 +83,38 @@
     </div>
 
     <div class="lg:pl-72">
-      <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+      <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-700 bg-gray-800 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
         <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
           <span class="sr-only">Open sidebar</span>
           <Bars3Icon class="size-6" aria-hidden="true" />
         </button>
 
-        <!-- Separator -->
-        <div class="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
-
         <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-          <form class="grid flex-1 grid-cols-1" action="#" method="GET">
-            <input type="search" name="search" aria-label="Search" class="col-start-1 row-start-1 block size-full bg-white pl-8 text-base text-gray-900 outline-none placeholder:text-gray-400 sm:text-sm/6" placeholder="Search" />
-            <MagnifyingGlassIcon class="pointer-events-none col-start-1 row-start-1 size-5 self-center text-gray-400" aria-hidden="true" />
-          </form>
-          <div class="flex items-center gap-x-4 lg:gap-x-6">
+          <!-- Home button -->
+          <div class="grid flex-1 grid-cols-1">
             <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="size-6" aria-hidden="true" />
+                <HomeIcon class="size-6" aria-hidden="true" />
             </button>
+          </div>
+
+          <div class="flex items-center gap-x-4 lg:gap-x-6">
+            <!-- Previous Lesson -->
+            <div class="mt-0 flex items-center gap-x-6">
+              <a href="#" class="text-sm/6 font-bold text-white">
+                <span aria-hidden="true">←</span>
+                Previous Lesson 
+              </a>
+            </div>
 
             <!-- Separator -->
-            <div class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true" />
+            <div class="hidden lg:block lg:h-6 lg:w-px bg-gray-700" aria-hidden="true" />
 
-            <!-- Profile dropdown -->
-            <Menu as="div" class="relative">
-              <MenuButton class="-m-1.5 flex items-center p-1.5">
-                <span class="sr-only">Open user menu</span>
-                <img class="size-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                <span class="hidden lg:flex lg:items-center">
-                  <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true">Tom Cook</span>
-                  <ChevronDownIcon class="ml-2 size-5 text-gray-400" aria-hidden="true" />
-                </span>
-              </MenuButton>
-              <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                <MenuItems class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                  <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                    <a :href="item.href" :class="[active ? 'bg-gray-50 outline-none' : '', 'block px-3 py-1 text-sm/6 text-gray-900']">{{ item.name }}</a>
-                  </MenuItem>
-                </MenuItems>
-              </transition>
-            </Menu>
+            <!-- Complete and Continue button here -->
+            <button type="button" 
+              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Complete and go to next lesson <span aria-hidden="true">→</span>
+            </button>
           </div>
         </div>
       </div>
@@ -154,17 +159,26 @@ import {
   UsersIcon,
   XMarkIcon,
 } from '@heroicons/vue/24/outline'
-import { ChevronDownIcon, MagnifyingGlassIcon, CheckIcon, CheckCircleIcon, CircleIcon } from '@heroicons/vue/20/solid'
+import { ChevronDownIcon, MagnifyingGlassIcon, CheckIcon, CheckCircleIcon, PlayCircleIcon } from '@heroicons/vue/20/solid'
 
 const videoId = 'e1905766c033f9f40fc910a8756f08f0'
+
+const chapters = [
+  { name: 'Chapter #1', duration: '10:00' },
+  { name: 'Chapter #2', duration: '10:00' },
+  { name: 'Chapter #3', duration: '10:00' },
+  { name: 'Chapter #4', duration: '10:00' },
+  { name: 'Chapter #5', duration: '10:00' },
+  { name: 'Chapter #6', duration: '10:00' },
+]
 
 const navigation = [
   { name: 'Lesson #1 (00:00)', href: '#', icon: CheckCircleIcon, current: true },
   { name: 'Lesson #2 (11:11)', href: '#', icon: UsersIcon, current: false },
-  { name: 'Lesson #3 (22:22)', href: '#', icon: FolderIcon, current: false },
+  { name: 'Lesson #3 (22:22)', href: '#', icon: PlayCircleIcon, current: false },
   { name: 'Lesson #4 (33:33)', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Lesson #5 (44:44)', href: '#', icon: CircleIcon, current: false },
-  { name: 'Lesson #6 (55:55)', href: '#', icon: CircleIcon, current: false },
+  { name: 'Lesson #5 (44:44)', href: '#', icon: CheckCircleIcon, current: false },
+  { name: 'Lesson #6 (55:55)', href: '#', icon: CheckCircleIcon, current: false },
 ]
 const teams = [
   { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
